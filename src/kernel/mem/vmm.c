0ca9955bbccd48e7afb509490182d49fb91988b5
Carlos <105173667+carlosdaniel26@users.noreply.github.com>
2025-01-08 18:17:12 -0300

    now making the C code readble

--- src/kernel/mem/vmm.c
+++ src/kernel/mem/vmm.c
@@ -1,38 +1,38 @@
 #include <stdint.h>
-#include <string.h>
 
-uint32_t page_directory[1024] __attribute__((aligned(4096)));
-uint32_t page_table[1024] __attribute__((aligned(4096)));
+#define ENTRY_AMMOUNT 1024
+#define PRESENT_WRITABLE 0x3
+#define PAGE_SIZE_BYTES 4096
 
-extern unsigned kernel_end;
+uint32_t page_directory[ENTRY_AMMOUNT] __attribute__((aligned(4096)));
+uint32_t page_table[ENTRY_AMMOUNT] __attribute__((aligned(4096)));
+
+extern uint32_t kernel_end;
 
 void enable_paging_c() 
 {
-    // Zero out the page directory and page table manually
-    memset(page_directory, 0, 4096);
-    memset(page_table, 0, 4096);
+    uint32_t page_ammount = (uint32_t)(&kernel_end) / 4096;
+    uint32_t address = 0x0; // First P_Address to map
 
-    // Set up page table for 0x100000 to 0x128000
-    uint32_t *edi = page_table;
-    uint32_t ecx = (unsigned)(&kernel_end) / 4096; // Number of pages
-    uint32_t eax = 0x0; // First P_Address to map
+    for (uint32_t i = 0; i < page_ammount; i++) 
+    {
+        uint32_t page_dir_index = address >> 12; // Get the page number
 
-    for (uint32_t i = 0; i < ecx; i++) {
-        uint32_t ebx = eax; // Save the address for calculations
-        ebx >>= 12; // Get the page number
+        uint32_t entry = address;
+        entry |= PRESENT_WRITABLE;
+        page_table[page_dir_index] = entry;
 
-        uint32_t edx = eax; // Save the address for entry
-        edx |= 0x3; // Mark as present and writable
-
-        edi[ebx] = edx; // Write the entry to the page table
-        eax += 0x1000; // Move to the next page
+        address += PAGE_SIZE_BYTES;
     }
+}
 
+void enable_paging_registers()
+{
     // Link page table in page directory
     uint32_t *page_dir = page_directory;
-    page_dir[0] = (uint32_t)page_table | 0x3; // Mark as present and writable
+    page_dir[0] = (uint32_t)page_table | PRESENT_WRITABLE;
 
-    // Load the page directory into CR3
+    // Load the page directory
     __asm__ volatile (
         "mov %0, %%eax\n"
         "mov %%eax, %%cr3\n"
@@ -41,7 +41,7 @@ void enable_paging_c()
         : "%eax"
     );
 
-    // Enable paging by setting PG bit in CR0
+    // Enable paging in CR0
     __asm__ volatile (
         "mov %%cr0, %%eax\n"
         "or $0x80000001, %%eax\n"
