d62ee9b4b012b5a729cbd7b8439f1216643a3fb0
Carlos Daniel <105173667+carlosdaniel26@users.noreply.github.com>
2025-01-24 13:49:24 -0500

    making it now it install qemu too; making it simplier

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,16 +1,8 @@
-# This script installs the dependencies required to build the GCC compiler.
+# install dependencies
+sudo apt install -y build-essential bison flex libgmp3-dev libmpc-dev libmpfr-dev texinfo libisl-dev qemu-system-x86
 
-sudo apt install build-essential &&
-sudo apt install bison &&
-sudo apt install flex &&
-sudo apt install libgmp3-dev &&
-sudo apt install libmpc-dev  &&
-sudo apt install libmpfr-dev &&
-sudo apt install texinfo &&
-sudo apt install libisl-dev  &&
-
-mkdir tmp
-cd tmp
+# create temporary directory and navigate into it
+mkdir -p tmp && cd tmp
 
 # download sources
 wget https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.gz
@@ -20,29 +12,31 @@ wget https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.gz
 tar -xvf gcc-12.2.0.tar.gz
 tar -xvf binutils-2.39.tar.gz
 
-# constants 
+# set constants
 export PREFIX="$HOME/opt/cross"
 export TARGET=i686-elf
 export PATH="$PREFIX/bin:$PATH"
 
-# build dir
-mkdir build-binutils
-mkdir build-gcc
+# create build directories
+mkdir -p build-binutils build-gcc
 
-# build binutils
+# build and install binutils
+cd build-binutils
 ../binutils-2.39/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
-make -j4
+make -j$(nproc)
 make install
+cd ..
 
-# build gcc
+# build and install GCC
+cd build-gcc
 ../gcc-12.2.0/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers --disable-hosted-libstdcxx
-make all-gcc
-make all-target-libgcc
-make all-target-libstdc++-v3
+make -j$(nproc) all-gcc
+make -j$(nproc) all-target-libgcc
+make -j$(nproc) all-target-libstdc++-v3
 make install-gcc
 make install-target-libgcc
 make install-target-libstdc++-v3
+cd ..
 
 # test if it worked
-$HOME/opt/cross/bin/$TARGET-gcc --version
-export PATH="$HOME/opt/cross/bin:$PATH"
+$PREFIX/bin/$TARGET-gcc --version
