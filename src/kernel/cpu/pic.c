12913efa942ee76f95879fa8a693e9d62aa8cf50
Carlos Daniel <105173667+carlosdaniel26@users.noreply.github.com>
2025-02-03 08:41:37 -0300

    Revert "Revert "converting spaces to tabs; improving comments and using more macros to make code easier to read""
    This reverts commit 7939a28b81bfa0f2669c47472cee283f17c2dfdf.

--- src/kernel/cpu/pic.c
+++ src/kernel/cpu/pic.c
@@ -2,18 +2,11 @@
 #include <kernel/utils/io.h>
 #include <kernel/arch/idt.h>
 
-#define PIC1_COMMAND    	0x20
-#define PIC1_DATA       	0x21
-#define PIC2_COMMAND    	0xA0
-#define PIC2_DATA       	0xA1
-#define PIC_EOI         	0x20
-
-#define PIC_INIT_COMMAND 	0x11
-#define PIC_CASCADE_CONFIG 	0x04
-#define PIC_MODE_CONFIG 	0x01
-
-#define PIC1_OFFSET	32
-#define PIC2_OFFSET	40
+#define PIC1_COMMAND    0x20 
+#define PIC1_DATA       0x21
+#define PIC2_COMMAND    0xA0 
+#define PIC2_DATA       0xA1
+#define PIC_EOI         0x20
 
 extern void *isr_stub_table[];
 
@@ -22,39 +15,37 @@ extern void *isr_stub_table[];
  * By default, the interrupts are (0-15), while the CPU exceptions have a range between (0-31).
  * Here we remap the IRQs to 32-47.
  */
-
 void PIC_remap() 
 {
-	// Start initialization of PIC
-	outb(PIC1_COMMAND, PIC_INIT_COMMAND);
-	outb(PIC2_COMMAND, PIC_INIT_COMMAND);
+    // ICW1: Start initialization of PIC
+    outb(PIC1_COMMAND, 0x11); // Master PIC
+    outb(PIC2_COMMAND, 0x11); // Slave PIC
 
-	/* interrupt vector offsets */
-	outb(PIC1_DATA, PIC1_OFFSET); // (IRQ0-7) -> (IRQ 32-39)
-	outb(PIC2_DATA, PIC2_OFFSET); // Slave PIC vector offset (IRQ 40-47)
+    // ICW2: Set interrupt vector offsets
+    outb(PIC1_DATA, 0x20); // Master PIC vector offset (IRQ0-7)
+    outb(PIC2_DATA, 0x28); // Slave PIC vector offset (IRQ8-15)
 
-	/* Tell Master PIC there is a slave PIC at IRQ2 (0000 0100) */
-	outb(PIC1_DATA, PIC_CASCADE_CONFIG); // Master PIC
-	
-	/* Tell Slave PIC its cascade identity (0000 0010) */
-	outb(PIC2_DATA, 0x02); // Slave PIC
+    // ICW3: Tell Master PIC there is a slave PIC at IRQ2 (0000 0100)
+    outb(PIC1_DATA, 0x04); // Master PIC
+    // Tell Slave PIC its cascade identity (0000 0010)
+    outb(PIC2_DATA, 0x02); // Slave PIC
 
-	/* Set PIC to x86 mode */
-	outb(PIC_MODE_CONFIG, 0x01); // Master PIC
-	outb(PIC_MODE_CONFIG, 0x01); // Slave PIC
+    // ICW4: Set PIC to x86 mode
+    outb(PIC1_DATA, 0x01); // Master PIC
+    outb(PIC2_DATA, 0x01); // Slave PIC
 
-	/* Unmask all interrupts on the PIC */
-	outb(PIC1_DATA, 0xF9);
-	outb(PIC2_DATA, 0xFC);
+    // Unmask all interrupts on the PIC
+    outb(PIC1_DATA, 0xF9); // Master PIC
+    outb(PIC2_DATA, 0xFC); // Slave PIC
 }
 
 void PIC_sendEOI(uint8_t irq)
 {
-	// The Slave PIC has to be warned?
-	if (irq >= 8) {
-		outb(PIC2_COMMAND, PIC_EOI);
-	}
+    // The Slave PIC has to be warned?
+    if (irq >= 8) {
+        outb(PIC2_COMMAND, PIC_EOI);
+    }
 
-	// Warn the Master PIC
-	outb(PIC1_COMMAND, PIC_EOI);
+    // Warn the Master PIC
+    outb(PIC1_COMMAND, PIC_EOI);
 }
