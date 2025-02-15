170f47833c6209cc0758fba13e7f9cb480f04e7c
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-02-14 21:48:07 -0300

    echo on the scripts

--- scripts/space2tab.sh
+++ scripts/space2tab.sh
@@ -1,1 +1,3 @@
+echo "converting spaces to tabs.."
+
 find ../src ../include -type f -name "*.c" -o -name "*.h" | xargs sed -i 's/    /\t/g'
--- scripts/trim_spaces.sh
+++ scripts/trim_spaces.sh
@@ -1,1 +1,3 @@
+echo "trim spaces"
+
 find ../src ../include -type f -name "*.c" -o -name "*.h" | xargs sed -i 's/[ \t]*$//'
