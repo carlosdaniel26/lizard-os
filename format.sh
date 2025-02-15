709a5d44332d639b5f739533835a811ca1f3feab
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-02-15 00:16:07 -0300

    using reticence on format script echo now

--- format.sh
+++ format.sh
@@ -3,8 +3,8 @@
 echo "converting spaces to tabs.."
 find ./src ./include -type f -name "*.c" -o -name "*.h" | xargs sed -i 's/    /\t/g'
 
-echo "trim spaces"
+echo "trim spaces.."
 find ./src ./include -type f -name "*.c" -o -name "*.h" | xargs sed -i 's/[ \t]*$//'
 
-echo "formating comments"
+echo "formating comments.."
 find ./src ./include -type f \( -name "*.h" -o -name "*.c" \) -exec sed -i -E 's|//(.*)|/*\1*/|' {} +
