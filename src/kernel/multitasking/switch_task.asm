0cccf46e817fb6aff33f9776f151c85042349b59
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-02-15 16:31:21 -0300

    fixing registers offset macros nomenclature;  initializing all the struct correctly; allocating stack area for the new task

--- src/kernel/multitasking/switch_task.asm
+++ src/kernel/multitasking/switch_task.asm
@@ -1,3 +1,5 @@
+extern pmm_alloc_block
+
 %define MAX_TASKS 8
 %define STACK_END (DEFAULT_STACK_SIZE - 1)
 
@@ -8,16 +10,16 @@
 %define NEXT_TASK             8
 %define STATE                 12
 
-%define EAX            32
-%define EBX            36
-%define ECX            40
-%define EDX            44
-%define ESI            48
-%define EDI            52
-%define EBP            56
-%define ESP            60
-%define EIP            64
-%define EFLAGS         68
+%define r_EAX            32
+%define r_EBX            36
+%define r_ECX            40
+%define r_EDX            44
+%define r_ESI            48
+%define r_EDI            52
+%define r_EBP            56
+%define r_ESP            60
+%define r_EIP            64
+%define r_EFLAGS         68
 
 %define SCHEDULING_POLICY     16
 %define SCHEDULING_PRIORITY   20
@@ -33,9 +35,31 @@ global tasks
 section .text:
 
 ; int create_task(struct task *task, void (*entry_point)(void));
+global create_task
 create_task:
-	mov eax, [ebp + 8]	; task
-	mov ebx, [ebp + 12]	; entry_point
+	; Clean task struct
+	
 
-	mov [eax + EIP], ebx
+	mov ecx, [ebp + 4]	; task
+	mov ebx, [ebp + 8]	; entry_point
+
+	; alloc stack
+	call pmm_alloc_block ; eax = addr
+
+
+	; Create State
+	mov [ecx + r_EIP], ebX		; task->EIP = entry_point
+	mov [ecx + r_EBP], eaX		; task->EBP = stack_bottom
+	mov [ecx + r_ESP], eax		; task->ESP = stack_bottom
+
+	; Initialize General Registers to 0
+	xor eax, eax
+	mov [ecx + r_EAX], eax
+	mov [ecx + r_EBX], eax
+	mov [ecx + r_ECX], eax
+	mov [ecx + r_EDX], eax
+	mov [ecx + r_ESI], eax
+	mov [ecx + r_EDI], eax
+
+
 	ret
