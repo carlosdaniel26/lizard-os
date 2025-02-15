1be30fd1b78285f6f9d0f8d901305e6311b30ebc
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-02-15 00:50:03 -0300

    solving tab; max_tasks now is 8, now the registers states are on the struct now

--- include/kernel/multitasking/task.h
+++ include/kernel/multitasking/task.h
@@ -3,24 +3,30 @@
 
 #include <stdint.h>
 
-#define MAX_TASKS 256
+#define MAX_TASKS 8
 #define DEFAULT_STACK_SIZE 1024
 #define STACK_END DEFAULT_STACK_SIZE - 1
 
 struct Task
 {
-  void *kernel_stack_top;
-  void *virtual_address_space;
+	void *kernel_stack_top;
+	void *virtual_address_space;
 
-  struct Task *next_task;
-  int state; /* (running, waiting, ready)*/
+	struct Task *next_task;
+	int state; /* (running, waiting, ready)*/
 
-  /* Optional fields*/
-  int scheduling_policy;   /* Task's scheduling policy*/
-  int scheduling_priority; /* Task's scheduling priority*/
-  int pid;				 /* Process ID the task belongs to*/
-  char task_name[64];	  /* Task name (for debugging/monitoring purposes)*/
-  unsigned long cpu_time_consumed; /* CPU time consumed by the task so far*/
+	unsigned long eax, ebx, ecx, edx;
+	unsigned long esi, edi, ebp;
+	unsigned long esp;			   /* Stack pointer*/
+	unsigned long eip;			   /* Instruction pointer*/
+	unsigned long eflags;			/* Flags register*/
+
+	/* Optional fields*/
+	int scheduling_policy;   /* Task's scheduling policy*/
+	int scheduling_priority; /* Task's scheduling priority*/
+	int pid;				 /* Process ID the task belongs to*/
+	char task_name[64];	  /* Task name (for debugging/monitoring purposes)*/
+	unsigned long cpu_time_consumed; /* CPU time consumed by the task so far*/
 };
 
 int create_task(struct task *task, void (*entry_point)(void));
