05f8aeab31f1542e2ddcbbeb16028ba93b7f6d7a
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-02-16 20:34:33 -0300

    solving magic number bug

--- src/boot.asm
+++ src/boot.asm
@@ -1,37 +1,29 @@
 section .bss
-	align 16
+align 16
 stack_bottom:
-	resb 16384 * 8  ; 128 KiB
-stack_top: ; after the 128 KiB reserved for the stack
+    resb 16384 * 8  ; 128 KiB
 
-section .text
-	global _start
-_start:
-	align 8
-
-	; Multiboot2 header
+section .multiboot
+align 8
 multiboot2_header_start:
-	dd 0xe85250d6         ; Magic number
-	dd 0                  ; Architecture (0 for x86)
-	dd multiboot2_header_end - multiboot2_header_start ; Header length
-	dd -(0xe85250d6 + 0 + (multiboot2_header_end - multiboot2_header_start)) ; Checksum
+    dd 0xe85250d6
+    dd 0
+    dd multiboot2_header_end - multiboot2_header_start
+    dd -(0xe85250d6 + 0 + (multiboot2_header_end - multiboot2_header_start))
 multiboot2_header_end:
 
-	; Tag: End
-	align 8
-	dw 0                  ; Type (END tag)
-	dw 0                  ; Flags (no flags)
-	dd 8                  ; Tag size (W + W + L = 8)
+    dw 0
+    dw 0
+    dd 8
 
-	; Configure stack
-	mov esp, stack_top
+section .text
+global _start
+_start:
+    mov esp, stack_bottom + (16384 * 8)
+    push ebx
+    push eax
+    extern kernel_main
+    call kernel_main
 
-	push ebx              ; multiboot_info
-	push eax              ; magic_number
-
-	; Call the kernel
-	extern kernel_main
-	call kernel_main
-
-times 510-($-$$) db 0  ; Fill up to 510 bytes
-dw 0xAA55             ; Boot signature
+    cli
+    hlt
