8190662768506413bb444dfd5d6d7fd23612f7d8
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-02-16 22:47:32 -0300

    Fixing task struct offset; Create state function working now

--- src/kernel/multitasking/switch_task.asm
+++ src/kernel/multitasking/switch_task.asm
@@ -7,28 +7,29 @@ extern pmm_alloc_block
 
 %define KERNEL_STACK_TOP      0
 %define VIRTUAL_ADDRESS_SPACE  4
+
 %define NEXT_TASK             8
 %define STATE                 12
 
-%define r_EAX            32
-%define r_EBX            36
-%define r_ECX            40
-%define r_EDX            44
-%define r_ESI            48
-%define r_EDI            52
-%define r_EBP            56
-%define r_ESP            60
-%define r_EIP            64
-%define r_EFLAGS         68
+%define r_EAX            16
+%define r_EBX            20
+%define r_ECX            24
+%define r_EDX            28
+%define r_ESI            32
+%define r_EDI            36
+%define r_EBP            40
+%define r_ESP            44
+%define r_EIP            48
+%define r_EFLAGS         52
 
-%define SCHEDULING_POLICY     16
-%define SCHEDULING_PRIORITY   20
-%define PID                   24
-%define NAME                  28
-%define CPU_TIME_CONSUMED     92
-
+%define SCHEDULING_POLICY     56
+%define SCHEDULING_PRIORITY   60
+%define PID                   64
+%define NAME                  68
+%define CPU_TIME_CONSUMED     132
 
 %define TASK_SIZE (CPU_TIME_CONSUMED + 4)
+
 
 global tasks
 
@@ -37,29 +38,27 @@ section .text:
 ; int create_task(struct task *task, void (*entry_point)(void));
 global create_task
 create_task:
-	; Clean task struct
-	
+    push ebp
+    mov ebp, esp
 
-	mov ecx, [ebp + 4]	; task
-	mov ebx, [ebp + 8]	; entry_point
+    ; Params
+    mov ecx, [ebp + 8]    ; task
+    mov edx, [ebp + 12]   ; entry_point
 
-	; alloc stack
-	call pmm_alloc_block ; eax = addr
+    ; alloc stack (eax = stack_bottom)
+    push ecx
+    call pmm_alloc_block
+    pop ecx
 
+    ; Go to End of the Page
+    add eax, 4095
 
-	; Create State
-	mov [ecx + r_EIP], ebX		; task->EIP = entry_point
-	mov [ecx + r_EBP], eaX		; task->EBP = stack_bottom
-	mov [ecx + r_ESP], eax		; task->ESP = stack_bottom
+    ; Create State
 
-	; Initialize General Registers to 0
-	xor eax, eax
-	mov [ecx + r_EAX], eax
-	mov [ecx + r_EBX], eax
-	mov [ecx + r_ECX], eax
-	mov [ecx + r_EDX], eax
-	mov [ecx + r_ESI], eax
-	mov [ecx + r_EDI], eax
+    mov dword [ecx + r_ESP], eax    ; ESP = stack_bottom
+    mov dword [ecx + r_EBP], eax    ; ESP = stack_bottom
 
+    mov dword [ecx + r_EIP], edx    ; EIP = entry_point
 
-	ret
+    pop ebp
+    ret
