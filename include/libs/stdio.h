c30590c03457f5f0409151cb3347d2f6755ccbe9
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-02-17 20:52:10 -0300

    removing useless struct

--- include/libs/stdio.h
+++ include/libs/stdio.h
@@ -4,13 +4,6 @@
 #define EOF (-1)
 typedef int (*pfnStreamWriteBuf)(char*);
 
-struct Stream {
-	size_t buf_len;
-	size_t buf_i;
-	char *buf;
-	pfnStreamWriteBuf pfn_write_all;
-};
-
 bool kprint(const char* data, size_t length);
 int kprintf(const char* __restrict, ...);
 int putchar(char character);
