4c01d4e8aae4fedc827ef9f97d8d630718f755aa
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-02-21 19:33:35 -0300

    cleaning terminal after init

--- src/kernel/kernel.c
+++ src/kernel/kernel.c
@@ -64,7 +64,7 @@ void kernel_main(unsigned long magic_number, unsigned long addr)
 	create_task(tasks, &cpuid_kprint, "cpuid_kprint");
 
 	kprint_task_state(tasks);
-	//terminal_clean();
+	terminal_clean();
 
 	shit_shell_init();
 
