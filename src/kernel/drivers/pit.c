7e13c0bde6b0d5d4075dc610693f839dba687874
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-04-21 00:08:31 -0300

    solving size type warning on pit frequency

--- src/kernel/drivers/pit.c
+++ src/kernel/drivers/pit.c
@@ -9,14 +9,15 @@
 #define PIT_COMMAND 0x43
 #define PIT_CHANNEL0 0x40
 #define PIT_FREQUENCY_HZ 1193182
+#define PIT_DESIRED_FREQUENCY_HZ PIT_FREQUENCY_HZ / 1000
 
 
 void pit_init()
 {
 	outb(PIT_COMMAND, 0b00110110);	  /* Mode 3, Channel 0, low/high byte acess*/
 
-	outb(PIT_CHANNEL0, PIT_FREQUENCY_HZ & 0xFF);	/* Low Byte */
-	outb(PIT_CHANNEL0, (PIT_FREQUENCY_HZ >> 8));	/* High Byte */
+	outb(PIT_CHANNEL0, PIT_DESIRED_FREQUENCY_HZ & 0xFF);	/* Low Byte */
+	outb(PIT_CHANNEL0, (PIT_DESIRED_FREQUENCY_HZ >> 8));	/* High Byte */
 }
 
 extern struct pt_regs ptrace;
