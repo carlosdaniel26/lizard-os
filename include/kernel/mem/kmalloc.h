8358dd358de16d7c4d9593f837b0a07e93176009
carlosdaniel26 <105173667+carlosdaniel26@users.noreply.github.com>
2025-04-21 00:08:12 -0300

    solving warning on attribute packed

--- include/kernel/mem/kmalloc.h
+++ include/kernel/mem/kmalloc.h
@@ -4,13 +4,13 @@
 #include <stddef.h>
 #include <stdbool.h>
 
-typedef struct KMemoryHeader {
+typedef struct __attribute__((packed)) KMemoryHeader {
 	struct KMemoryHeader* prev;
 	struct KMemoryHeader* next;
 	size_t size;
 	bool is_free;
 
-} KMemoryHeader __attribute__((packed));
+} KMemoryHeader;
 
 void* kmalloc(size_t n_bytes);
 void kfree(void* ptr);
