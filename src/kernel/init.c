e5bcd0ccc4aa9cef5ea94656e893f51fb8a63f06
Carlos Daniel <105173667+carlosdaniel26@users.noreply.github.com>
2025-05-12 22:38:36 -0300

    init terminal and identify on init system

--- src/kernel/init.c
+++ src/kernel/init.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <kernel/utils/alias.h>
 #include <kernel/shit-shell/ss.h>
+#include <kernel/terminal/tty.h>
 
 void init()
 {
+	tty_initialize();
 	debug_printf("PID1 on control\n");
+	ata_identify(1);
+
 	start_interrupts();
 	shit_shell_init();
 
