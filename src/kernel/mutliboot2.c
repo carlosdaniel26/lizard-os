bd811d5f5dadec658d615fe818c64c43fd0988a4
Carlos Daniel <105173667+carlosdaniel26@users.noreply.github.com>
2025-05-12 22:00:07 -0300

    removing kprintf from multiboot process since the videobuffer addr is found there so theres no sense on  let print before find

--- src/kernel/mutliboot2.c
+++ src/kernel/mutliboot2.c
@@ -10,84 +10,29 @@ struct multiboot_tag_mmap *mmap_tag;
 
 void process_multiboot2_tags(unsigned long magic_number, unsigned long addr)
 {
-	kprintf("Processing multiboot...\n");
-
 	if (magic_number != MULTIBOOT2_MAGIC)
 	{
-		kprintf("Invalid magic number: 0x%x\n", (unsigned) magic_number);
 		asm("hlt");
 		return;
 	}
 
 	if (addr & 7)
 	{
-		kprintf("Unaligned mbi: 0x%x\n", addr);
 		return;
 	}
 
 	unsigned size = *(unsigned *) addr;
-	kprintf("MBI size: %u\n", size);
-
-	tty_clean();
 	
 	for (tag = (struct multiboot_tag *) (addr + 8);
 		 tag->type != MULTIBOOT_TAG_TYPE_END;
 		 tag = (struct multiboot_tag *) ((uint8_t *) tag + ((tag->size + 7) & ~7)))
 	{
-		kprintf("Tag 0x%x, Size 0x%x\n", tag->type, tag->size);
 
 		switch (tag->type)
 		{
-			case MULTIBOOT_TAG_TYPE_CMDLINE:
-				kprintf("Command line = %s\n", ((struct multiboot_tag_string *) tag)->string);
-				break;
-			case MULTIBOOT_TAG_TYPE_BOOT_LOADER_NAME:
-				kprintf("Boot loader name = %s\n", ((struct multiboot_tag_string *) tag)->string);
-				break;
-			case MULTIBOOT_TAG_TYPE_MODULE:
-				kprintf("Module at 0x%x-0x%x. Command line %s\n",
-					   ((struct multiboot_tag_module *) tag)->mod_start,
-					   ((struct multiboot_tag_module *) tag)->mod_end,
-					   ((struct multiboot_tag_module *) tag)->cmdline);
-				break;
-			case MULTIBOOT_TAG_TYPE_BASIC_MEMINFO:
-				kprintf("mem_lower = %uKB, mem_upper = %uKB\n",
-					   ((struct multiboot_tag_basic_meminfo *) tag)->mem_lower,
-					   ((struct multiboot_tag_basic_meminfo *) tag)->mem_upper);
-
-					mem_ammount_kb =
-						((struct multiboot_tag_basic_meminfo *) tag)->mem_lower
-						+
-						((struct multiboot_tag_basic_meminfo *) tag)->mem_upper;
-
-
-
-				break;
-			case MULTIBOOT_TAG_TYPE_BOOTDEV:
-				kprintf("Boot device 0x%x,%u,%u\n",
-					   ((struct multiboot_tag_bootdev *) tag)->biosdev,
-					   ((struct multiboot_tag_bootdev *) tag)->slice,
-					   ((struct multiboot_tag_bootdev *) tag)->part);
-				break;
 			case MULTIBOOT_TAG_TYPE_MMAP:
-			{
 				mmap_tag = (struct multiboot_tag_mmap *) tag;
-				struct multiboot_mmap_entry *mmap;
-				kprintf("mmap\n");
-				for (mmap = mmap_tag->entries;
-					 (unsigned char *) mmap < (unsigned char *) tag + tag->size;
-					 mmap = (struct multiboot_mmap_entry *) ((unsigned long) mmap + mmap_tag->entry_size))
-				{
-					kprintf(" addr = 0x%x%x, len = 0x%x%x, t = 0x%x\n",
-						   (unsigned) (mmap->addr >> 32),
-						   (unsigned) (mmap->addr & 0xffffffff),
-						   (unsigned) (mmap->len >> 32),
-						   (unsigned) (mmap->len & 0xffffffff),
-						   (unsigned) mmap->type);
-
-				}
-			}
-			break;
+				break;
 
 
 			case MULTIBOOT_TAG_TYPE_FRAMEBUFFER:
@@ -99,7 +44,6 @@ void process_multiboot2_tags(unsigned long magic_number, unsigned long addr)
 			break;
 
 			default:
-				/*kprintf("Unknown tag type: 0x%x\n", tag->type);*/
 				break;
 		}
 	}
