d97a38192a30f082cae378067ac0f3d7217ad5d3
Carlos Daniel <105173667+carlosdaniel26@users.noreply.github.com>
2025-05-12 22:38:13 -0300

    now ata identify can read the model name

--- src/kernel/drivers/ata.c
+++ src/kernel/drivers/ata.c
@@ -20,15 +20,34 @@
 uint16_t base[] = {0, ATA_PRIMARY_BASE, ATA_SECONDARY_BASE};
 uint16_t ctrl[] = {0, ATA_PRIMARY_CTRL, ATA_SECONDARY_CTRL};
 
+#define MODEL_NAME_SIZE 41
+
+char primary_model[MODEL_NAME_SIZE];
+char secondary_model[MODEL_NAME_SIZE];
+char *model;
+
 static inline void ata_select(uint8_t drive_id)
 {
-	if (drive_id != PRIMARY || drive_id != SECONDARY)
+	if (drive_id != PRIMARY && drive_id != SECONDARY)
 	{
 		debug_printf("Invalid drive_id: %u\n", drive_id);
 		return;
 	}
 
-	outb(base[drive_id] + ATA_REG_DRIVE, 0xB0);
+	outb(base[drive_id] + ATA_REG_DRIVE, 0xA0);
+
+    switch(drive_id)
+    {
+        case PRIMARY:
+            model = primary_model;
+            break;
+
+        case SECONDARY:
+            model = secondary_model;
+            break;
+    }
+
+    io_wait();
 
 }
 
@@ -48,12 +67,38 @@ uint16_t ata_identify(uint8_t drive_id)
 
     outb(base[drive_id] + ATA_REG_COMMAND, CMD_IDENTIFY);
 
-    uint8_t status = 0x00;
+    uint8_t status;
     
-    while (status & 0x80); 
+    do
     {
         status = inb(base[drive_id] + ATA_REG_STATUS);
+    } while (status & 0x80);
+
+    if (status & 0x01) {
+        debug_printf("ATA Error on drive %u\n", drive_id);
+        return 0;
     }
+
+    if (!(status & 0x08)) {
+        debug_printf("DRQ not set; device not ready\n");
+        return 0;
+    }
+
+    uint16_t identify_data[256];
+    for (int i = 0; i < 256; ++i) 
+    {
+        identify_data[i] = inw(base[drive_id]);
+    }
+
+    for (int i = 0; i < 20; i++) 
+    {
+        model[i * 2] = identify_data[27 + i] >> 8;
+        model[i*2 + 1] = identify_data[27 + i] & 0xFF;
+    }
+
+    model[40] = '\0';
+    debug_printf("Drive model: %s\n", model);
+
 
     return status;
 }
