2262c775e1b26790472144f4545539878bc475b9
Carlos Daniel <105173667+carlosdaniel26@users.noreply.github.com>
2025-05-13 17:19:09 -0300

    ata_read_sector header

--- include/kernel/drivers/ata.h
+++ include/kernel/drivers/ata.h
@@ -2,3 +2,4 @@
 
 uint16_t ata_identify(uint8_t drive_id);
 int ata_write_sector(uint8_t drive_id, uint32_t lba, const char *buffer);
+int ata_read_sector(uint8_t drive_id, uint32_t lba, char *out);
