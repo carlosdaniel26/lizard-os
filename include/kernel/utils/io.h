ae74f30609e2258cd45c834498415d5bf31ff487
Carlos Daniel <105173667+carlosdaniel26@users.noreply.github.com>
2025-05-19 09:35:31 -0300

    macros for IO functions

--- include/kernel/utils/io.h
+++ include/kernel/utils/io.h
@@ -9,6 +9,16 @@ uint32_t inl(uint16_t port);
 void outb(uint16_t port, uint8_t value);
 void outw(uint16_t port, uint16_t value);
 void outl(uint16_t port, uint32_t value);
+
+#define out_u8(port, value)  outb((uint16_t)(port), (uint8_t)(value))
+#define out_u16(port, value) outw((uint16_t)(port), (uint16_t)(value))
+#define out_u32(port, value) outl((uint16_t)(port), (uint32_t)(value))
+
+#define in_u8(port, value)  inb((uint16_t)(port))
+#define in_u16(port, value) inw((uint16_t)(port))
+#define in_u32(port, value) inl((uint16_t)(port))
+
+
 void io_wait(void);
 
 #endif
