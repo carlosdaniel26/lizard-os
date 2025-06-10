901e8547cc1c5968d1d378834c08be5ab7359691
Carlos Daniel <105173667+carlosdaniel26@users.noreply.github.com>
2025-06-10 07:41:49 -0300

    removing build dir garbage

--- build/x86_64/kernel/x86_64/cc-runtime/src/cc-runtime.c.d
+++
@@ -1,2 +0,0 @@
-../build/x86_64/kernel/x86_64/cc-runtime/src/cc-runtime.c.o: \
- cc-runtime/src/cc-runtime.c
--- build/x86_64/kernel/x86_64/src/main.c.d
+++
@@ -1,2 +0,0 @@
-../build/x86_64/kernel/x86_64/src/main.c.o: src/main.c src/limine.h
-src/limine.h:
