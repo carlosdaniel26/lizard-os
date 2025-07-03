5518bcd041710e9b0a7558eff869113cd20411c2
Carlos <105173667+carlosdaniel26@users.noreply.github.com.>
2025-07-03 04:11:13 -0300

    fixing pmm.h

--- kernel/src/pmm.h
+++ kernel/src/pmm.h
@@ -7,9 +7,10 @@ typedef uint64_t block_id_t;
 
 void kprint_ammount_mem_mb();
 void pmm_init();
-void *pmm_alloc_blocks(block_id_t ammount);
+void *pmm_alloc_block();
 void pmm_reserve_block(block_id_t block_number);
 void pmm_free_block(void* ptr);
 block_id_t pmm_free_block_count();
+void pmm_test_all();
 
 #endif
