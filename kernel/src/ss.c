24b74d910169636886bef09ed53d80e498752de7
Carlos <105173667+carlosdaniel26@users.noreply.github.com.>
2025-07-03 04:11:42 -0300

    now lzfetch shows memory in MB

--- kernel/src/ss.c
+++ kernel/src/ss.c
@@ -79,7 +79,7 @@ static inline void lzfetch()
     kprintf("|    ^        /    \\         |\t\t\t\t\t\t\tTerminal: tty0\n");
     kprintf("|   /|       (      )        |\t\t\t\t\t\t\tTheme:    CalangoGreen\n");
     kprintf("|  / |     ,__\\    /__,      |\t\t\t\t\t\t\tCPU:      %s\n", cpu.brand_name);
-    kprintf("|  \\ \\   _//---,  ,--\\\\_     |\t\t\t\t\t\t\tRAM:      %u.%uGB\n", mem_ammount_mb / (1024));
+    kprintf("|  \\ \\   _//---,  ,--\\\\_     |\t\t\t\t\t\t\tRAM:      %u.%uMB\n", mem_ammount_mb);
     kprintf("|   \\ \\   /\\  /  /   /\\      | \n");
     kprintf("|    \\ \\.___,/  /            |\n");
     kprintf("|     \\.______,/             |\n");
