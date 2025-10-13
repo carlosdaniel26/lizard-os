2c1cd46b9cd6d0f082594d578cb43bd18671a61f
Carlos D. S. Silva <105173667+carlosdaniel26@users.noreply.github.com>
2025-10-13 19:36:59 -0300

    now the functions "fat16_read_file, find_in_root, find_in_dir, find_in_path" works properly

--- kernel/src/fat16.c
+++ kernel/src/fat16.c
@@ -46,6 +46,7 @@ static uint32_t cluster_to_lba(Fat16 *fs, uint16_t cluster)
 {
     return  ((cluster - 2) * fs->header.sectors_per_cluster)
            + fs->data_region_lba;
+           
 }
 
 static int read_fat_entry(Fat16 *fs, uint16_t cluster, uint16_t *next) 
@@ -69,51 +70,43 @@ static uint16_t get_next_cluster(Fat16 *fs, uint16_t cluster)
     return next;
 }
 
-static void convert_name_to_83(const char *normal_name, char *out_83_name) 
-{	
-	/* Init */
-	memset(out_83_name, ' ', FILE_NAME_SIZE);
-	
-	/* Get data */
-	const char *dot = strchr(normal_name, '.');
-	size_t name_len;
-
-	if (dot)
-		name_len = (size_t)(dot - normal_name);
-	else
-		name_len = strlen(normal_name);
-	
-	/* truncate */
-	if (name_len > 8) name_len = 8;
-
-	/* copy actual name */
-	memcpy(out_83_name, normal_name, name_len);
-
-	/* If has extension, copy it */
-	if (dot)
-	{
-		/* Get the extension len ex: file.[txt] the "txt" len is 3 */
-		size_t ext_len = strlen(dot + 1);
-
-		/* Truncate */
-		if (ext_len > 3) ext_len = 3;
-
-		/*The extension is fixed position, so copy the text after the dot to the end of the 8.3 name*/
-		memcpy(out_83_name + 8, dot + 1, ext_len);
-	}
-
-	/* FAT 83 names are always upper case, then convert */
-	for (int i = 0; i < FILE_NAME_SIZE; i++)
-		out_83_name[i] = toupper((unsigned char)out_83_name[i]);
+static void convert_83_to_string(const uint8_t name[8], const uint8_t ext[3], char *out_string)
+{
+    int pos = 0;
+    
+    for (int i = 0; i < 8; i++)
+    {
+        if (name[i] != ' ')
+            out_string[pos++] = name[i];
+    }
+    
+    if (ext[0] != ' ')
+    {
+        out_string[pos++] = '.';
+        
+        for (int i = 0; i < 3; i++)
+        {
+            if (ext[i] != ' ')
+                out_string[pos++] = ext[i];
+        }
+    }
+    
+    out_string[pos] = '\0';
 }
 
 static int compare_filenames(const char *filename, const Fat16Directory *entry)
 {
-    char formated[FILE_NAME_SIZE];
+    char filename_copy[strlen(filename) + 1];
+    strcpy(filename_copy, filename);
 
-	convert_name_to_83(filename, formated);
+    for(int i = 0; i < strlen(filename_copy); i++)
+       filename_copy[i] = toupper(filename_copy[i]);
 
-	return (memcmp(formated, entry->name, FILE_NAME_SIZE) == 0);
+    char entry_name[13];
+    
+    convert_83_to_string(entry->name, entry->extension, entry_name);
+
+    return (strcmp(filename_copy, entry_name));
 }
 
 int fat16_read_file(Fat16* fs, Fat16Directory* entry, char* buffer)
@@ -133,16 +126,12 @@ int fat16_read_file(Fat16* fs, Fat16Directory* entry, char* buffer)
                              (fs->header.root_entry_count * 32 + 511) / 512;
 
     char sector_buffer[512];
-    
-    kprintf("Reading file: clusters=%u, size=%u bytes\n", 
-            current_cluster, file_size);
+
     
     while (current_cluster >= 0x0002 && current_cluster <= 0xFFEF && bytes_read < file_size)
     {
         uint32_t cluster_lba = data_start_lba + 
                               (current_cluster - 2) * sectors_per_cluster;
-        
-        kprintf("Reading cluster %u at LBA %u\n", current_cluster, cluster_lba);
         
         for (int i = 0; i < sectors_per_cluster && bytes_read < file_size; i++) 
         {
@@ -158,28 +147,21 @@ int fat16_read_file(Fat16* fs, Fat16Directory* entry, char* buffer)
             
             memcpy(buffer + bytes_read, sector_buffer, bytes_to_copy);
             bytes_read += bytes_to_copy;
-            
-            kprintf("Read %u bytes from sector %u (total: %u/%u)\n", 
-                   bytes_to_copy, cluster_lba + i, bytes_read, file_size);
         }
         
         current_cluster = get_next_cluster(fs, current_cluster);
         
         if (current_cluster == 0xFFFF) {
-            kprintf("End of file cluster chain\n");
             break;
         }
     }
     
-    kprintf("File read completed: %u bytes read\n", bytes_read);
     return 0;
 }
 
 int find_in_root(Fat16 *fs, const char *filename, Fat16Directory *out)
 {
 	char buffer[512];
-	char formated_filename[FILE_NAME_SIZE];
-	convert_name_to_83(filename, formated_filename);
 
 	uint32_t root_dir_entries = fs->header.root_entry_count;
 	uint32_t root_dir_size = root_dir_entries * FAT16_DIR_ENTRY_SIZE;
@@ -195,14 +177,13 @@ int find_in_root(Fat16 *fs, const char *filename, Fat16Directory *out)
 		{
 			Fat16Directory entry;
 			memcpy(&entry, buffer + (i * FAT16_DIR_ENTRY_SIZE), sizeof(Fat16Directory));
-
+            
 			if (! is_valid_entry(&entry))
 				continue;
 
-			if (compare_filenames(formated_filename, &entry) == 0)
+			if (compare_filenames(filename, &entry) == 0)
 			{
 				memcpy(out, &entry, sizeof(Fat16Directory));
-
 				return 0; /* Found */
 			}
 		}
@@ -213,34 +194,39 @@ int find_in_root(Fat16 *fs, const char *filename, Fat16Directory *out)
 
 int find_in_dir(Fat16 *fs, uint16_t start_cluster, const char *filename, Fat16Directory *out)
 {
-	char formated[FILE_NAME_SIZE];
-	convert_name_to_83(filename, formated);
-	uint16_t current_cluster = start_cluster;
-	char buffer[512];
+    uint16_t current_cluster = start_cluster;
+    char buffer[512];
+    size_t total_entries_checked = 0;
 
-	do {
-		uint32_t lba = cluster_to_lba(fs, current_cluster);
+    do {
+        uint32_t lba = cluster_to_lba(fs, current_cluster);
 
-		for (uint8_t sector = 0; sector < fs->header.sectors_per_cluster; sector++) 
-		{
-			if (atapio_read_sector(fs->disk, lba + sector, buffer) != 0)
-				return -1;
+        for (uint8_t sector = 0; sector < fs->header.sectors_per_cluster; sector++) 
+        {
+            if (atapio_read_sector(fs->disk, lba + sector, buffer) != 0)
+                return -1;
 
-			for (uint16_t i = 0; i < fs->header.bytes_per_sector / FAT16_DIR_ENTRY_SIZE; i++) {
-				Fat16Directory entry;
-				memcpy(&entry, buffer + (i * FAT16_DIR_ENTRY_SIZE), sizeof(Fat16Directory));
+            for (uint16_t i = 0; i < fs->header.bytes_per_sector / FAT16_DIR_ENTRY_SIZE; i++) 
+            {
+                Fat16Directory entry;
+                memcpy(&entry, buffer + (i * FAT16_DIR_ENTRY_SIZE), sizeof(Fat16Directory));
+                
+                if(!is_valid_entry(&entry))
+                    continue;
 
-				if (is_valid_entry(&entry) && compare_filenames(formated, &entry) == 0) {
-					memcpy(out, &entry, sizeof(Fat16Directory));
-					return 0; /* Found */
-				}
-			}
-		}
-		
-		current_cluster = get_next_cluster(fs, current_cluster);
-	} while (current_cluster < FAT16_EOC);
+                total_entries_checked++;
 
-	return -1; /* Not found */
+                if (compare_filenames(filename, &entry) == 0) {
+                    memcpy(out, &entry, sizeof(Fat16Directory));
+                    return 0; /* Found */
+                }
+            }
+        }
+        
+        current_cluster = get_next_cluster(fs, current_cluster);
+    } while (current_cluster < FAT16_EOC);
+
+    return -1; /* Not found */
 }
 
 int find_in_path(Fat16 *fs, const char *path, Fat16Directory *out)
@@ -305,15 +291,6 @@ int find_in_path(Fat16 *fs, const char *path, Fat16Directory *out)
             p++;
     }
 
-    /** Print tokens with count */
-    kprintf("=== TOKENS FOUND ===\n");
-    kprintf("Total tokens: %u\n", token_index);
-    kprintf("Original path: '%s'\n", path);
-    for (int i = 0; i < token_index; i++) {
-        kprintf("Token [%u/%u]: '%s'\n", i + 1, token_index, tokens[i]);
-    }
-    kprintf("====================\n");
-
     /** Search for each token in filesystem */
     int is_root = 1;
 
@@ -327,18 +304,15 @@ int find_in_path(Fat16 *fs, const char *path, Fat16Directory *out)
 
         if (is_root) 
 		{
-            kprintf("Searching in ROOT: %s\n", tokens[i]);
             result = find_in_root(fs, tokens[i], &entry);
             is_root = 0;
-        } else {
-            kprintf("Searching in cluster %u: %s\n",
-                    current_entry.first_cluster_low, tokens[i]);
+        } else 
+        {
             result = find_in_dir(fs, current_entry.first_cluster_low, tokens[i], &entry);
         }
 
         if (result != 0) 
 		{
-            kprintf("  -> '%s' not found\n", tokens[i]);
             /** Cleanup tokens before returning */
             for (int j = 0; j < N; j++) {
                 kfree(tokens[j]);
@@ -371,7 +345,7 @@ void test_vfs()
     }
 
     /* Read FAT16 Reader */
-    Fat16 fs;
+    Fat16 fs = {0};
     fs.disk = dev;
     char buffer[512] = {0};
     if (atapio_read_sector(dev, 0, &buffer) != 0) {
@@ -383,12 +357,15 @@ void test_vfs()
     /* Calculate root directory position */
     fs.fat_start_lba = fs.header.reserved_sector_count;
     fs.root_dir_lba = fs.fat_start_lba + (fs.header.num_fats * fs.header.fat_size_16);
+    fs.data_region_lba = fs.root_dir_lba + 
+                         (fs.header.root_entry_count * FAT16_DIR_ENTRY_SIZE + fs.header.bytes_per_sector - 1) / fs.header.bytes_per_sector;
+    fs.root_dir_sectors = (fs.header.root_entry_count * FAT16_DIR_ENTRY_SIZE + fs.header.bytes_per_sector - 1) / fs.header.bytes_per_sector;
 
-    kprintf("FAT16 Info: Root LBA=%u, FAT LBA=%u\n", fs.root_dir_lba, fs.fat_start_lba);
+    kprintf("FAT16 Info: Root LBA=%u, FAT LBA=%u, DATA LBA=%u\n", fs.root_dir_lba, fs.fat_start_lba, fs.data_region_lba);
 
     Fat16Directory entry;
 
-    char file_name[64] = "/CARLOS.TXT";
+    char file_name[64] = "/FOLDER/FILE.TXT";
     
     /* Test simple file first */
     kprintf("\n=== Testing %s ===\n", file_name);
@@ -398,7 +375,8 @@ void test_vfs()
     }
     else
     {
-        kprintf("SUCCESS: FILE NAME: %s, SIZE: %u bytes, CLUSTER: %u\n", 
+        kprintf("\n\n==========================");
+        kprintf("\nSUCCESS: FILE NAME: %s, SIZE: %u bytes, CLUSTER: %u\n", 
                 entry.name, entry.file_size_bytes, entry.first_cluster_low);
         
         char *file_content = (char*)kmalloc(entry.file_size_bytes + 1);
