37c402db9c26092fd6ff528a2c7c55d27fc3ed8b
Carlos D. S. Silva <105173667+carlosdaniel26@users.noreply.github.com>
2025-10-19 08:34:32 -0300

    fix: pmm unusable/usable blocks

--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -21,8 +21,12 @@ __attribute__((
 
 
 uint64_t mem_ammount_b = 0;
+uint64_t mem_usable_ammount_b = 0;
+uint64_t mem_unusable_ammount_b = 0;
 uint8_t *bitmap = NULL;
-uint64_t total_blocks = 0;
+uint32_t total_blocks = 0;
+uint32_t usable_blocks = 0;
+uint32_t unusable_blocks = 0;
 uint64_t hhdm_offset = 0;
 
 static inline void pmm_reserve_block(uint64_t i)
@@ -46,22 +50,45 @@ void pmm_init()
     for (uint64_t i = 0; i < memmap_request.response->entry_count; i++)
     {
         struct limine_memmap_entry *e = memmap_request.response->entries[i];
+        mem_ammount_b += e->length;
+
         if (e->type == LIMINE_MEMMAP_USABLE)
-            mem_ammount_b += e->length;
+            mem_usable_ammount_b += e->length;
     }
 
-    total_blocks = mem_ammount_b / BLOCK_SIZE;
+    mem_unusable_ammount_b = mem_ammount_b - mem_usable_ammount_b;
 
-    /* Init memory as reserved */
+    total_blocks = mem_ammount_b / BLOCK_SIZE;
+    usable_blocks = mem_usable_ammount_b / BLOCK_SIZE;
+    unusable_blocks = (mem_unusable_ammount_b + 1) / BLOCK_SIZE; /* Round up with the +1*/
+
+    uint64_t bitmap_size = (total_blocks + 7) / 8;  /* Ceiling division */
+
+    /* Find space for bitmap & Init memory as reserved */
     for (uint64_t i = 0; i < memmap_request.response->entry_count; i++)
     {
         struct limine_memmap_entry *e = memmap_request.response->entries[i];
-        if (e->type == LIMINE_MEMMAP_USABLE && e->length >= (total_blocks / 8 + 1))
+        if (e->type == LIMINE_MEMMAP_USABLE && e->length >= bitmap_size)
         {
             bitmap = (uint8_t *)(e->base + hhdm_offset);
-            memset(bitmap, 0xFF, total_blocks / 8 + 1);
+            memset(bitmap, 0xFF, bitmap_size);
+
+            /* As bitmap area will be fixed i'll count it as an unusable area */
+            uint64_t bitmap_blocks = (bitmap_size + BLOCK_SIZE - 1) / BLOCK_SIZE;
+            usable_blocks -= bitmap_blocks;
+            unusable_blocks += bitmap_blocks;
+
+            mem_unusable_ammount_b += bitmap_size;
+            mem_usable_ammount_b += bitmap_size;
+
             break;
         }
+    }
+
+    if (!bitmap) 
+    {
+        kprintf("PMM: ERROR - No suitable space for bitmap found!\n");
+        return;
     }
 
     /* Free the usable blocks */
@@ -88,17 +115,15 @@ void pmm_init()
 
     /* Protect Bitmap Area*/
     uintptr_t phys_bitmap = (uintptr_t)(bitmap - hhdm_offset);
-    uint64_t size = (total_blocks / 8) + 1;
-    uintptr_t end = ALIGN_UP(phys_bitmap + size, BLOCK_SIZE);
+    uintptr_t start_bitmap = ALIGN_UP(phys_bitmap, BLOCK_SIZE);
+    uintptr_t end_bitmap = ALIGN_UP(phys_bitmap + bitmap_size, BLOCK_SIZE);
 
-    for (uintptr_t addr = ALIGN_UP(phys_bitmap, BLOCK_SIZE); addr < end; addr += BLOCK_SIZE)
+    for (uintptr_t addr = start_bitmap; addr < end_bitmap; addr += BLOCK_SIZE)
     {
         uint64_t bid = addr / BLOCK_SIZE;
         if (bid < total_blocks)
             pmm_reserve_block(bid);
     }
-
-    // pmm_test_all();
 }
 
 void *pmm_alloc_block()
@@ -126,33 +151,28 @@ void *pmm_alloc_block_row(uint64_t ammount)
     if (ammount == 0)
         return NULL;
 
-    void *base = 0x00;
-    uint64_t base_block = 0;
-    uint64_t free_in_row = 0;
-
     for (uint64_t i = 0; i < total_blocks; i++)
     {
-        if (!pmm_test_block(i))
+        uint8_t found = 1;
+
+        for (uint64_t j = 0; j < ammount; j++)
         {
-            if (base == NULL)
+            if (pmm_test_block(i + j))
             {
-                base = (void *)(i * BLOCK_SIZE);
-                base_block = i;
-            }
-
-            free_in_row++;
-
-            if (free_in_row == ammount)
-            {
-                /* Reserve each block */
-                for (uint64_t block = base_block; block <= base_block; block++)
-                {
-                    pmm_reserve_block(block);
-                }
-
-                return (void *)((uint64_t)base);
+                found = 0;
+                i += j;
+                break;
             }
         }
+
+        if (found)
+        {
+            for (uint64_t j = 0; j < ammount; j++)
+                pmm_reserve_block(i + j);
+        }
+
+        return (void *)(i * BLOCK_SIZE);
+
     }
     return NULL;
 }
