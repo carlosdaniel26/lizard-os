d64d49ec5701996498e31a85716e52c5ea041482
Carlos D. S. Silva <105173667+carlosdaniel26@users.noreply.github.com>
2025-12-30 10:12:17 -0300

    Fix: Remove backslash-newline warning in debug.h
    The debug_printf macro in debug.h was causing a 'backslash-newline at end of file' warning. This commit puts the entire macro definition on a single line to resolve the issue.

--- kernel/src/debug.h
+++ kernel/src/debug.h
@@ -4,5 +4,4 @@
 #include <stdio.h>
 #include <clock.h>
 
-#define debug_printf(fmt, ...)																	   \
-	kprintf("[%u.%u] %s:%u: " fmt, g_uptime_seconds,pit_milliseconds,__FILE__, __LINE__, ##__VA_ARGS__)
+#define debug_printf(fmt, ...) kprintf("[%u.%u] %s:%u: " fmt, g_uptime_seconds,pit_milliseconds,__FILE__, __LINE__, ##__VA_ARGS__)
