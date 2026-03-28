5ceed0027e3c7f5ffbdb6fc5c79074ca3e9b3b21
Carlos D. S. Silva <105173667+carlosdaniel26@users.noreply.github.com>
2026-03-28 05:13:31 -0300

    chore: labels for initcalls on kmain

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -2,6 +2,7 @@
 #include <init.h>
 #include <kernelcfg.h>
 #include <limine.h>
+#include <pit.h>
 #include <stack.h>
 #include <stdbool.h>
 #include <types.h>
@@ -23,14 +24,14 @@ void kmain()
 {
     stack_init(kernel_stack, KERNEL_STACK_SIZE);
 
-    do_initcalls(__initcall0_start, __initcall0_end);
-    do_initcalls(__initcall1_start, __initcall1_end);
-    do_initcalls(__initcall2_start, __initcall2_end);
-    do_initcalls(__initcall3_start, __initcall3_end);
-    do_initcalls(__initcall4_start, __initcall4_end);
-    do_initcalls(__initcall5_start, __initcall5_end);
-    do_initcalls(__initcall6_start, __initcall6_end);
-    do_initcalls(__initcall7_start, __initcall7_end);
+    do_initcalls(__initcall0_start, __initcall0_end); /* early */
+    do_initcalls(__initcall1_start, __initcall1_end); /* core */
+    do_initcalls(__initcall2_start, __initcall2_end); /* postcore */
+    do_initcalls(__initcall3_start, __initcall3_end); /* arch */
+    do_initcalls(__initcall4_start, __initcall4_end); /* subsystem */
+    do_initcalls(__initcall5_start, __initcall5_end); /* filesystem */
+    do_initcalls(__initcall6_start, __initcall6_end); /* device */
+    do_initcalls(__initcall7_start, __initcall7_end); /* late */
 
     yield();
 }
